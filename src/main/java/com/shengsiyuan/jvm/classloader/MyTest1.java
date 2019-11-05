cfe3ce1d281ecbc23607a6cfe26ffb76fb7643b3
richard_glb <kilr1892@163.com>
2019-11-05 09:51:19 +0800

    解释

--- src/main/java/com/shengsiyuan/jvm/classloader/MyTest1.java
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest1.java
@@ -2,13 +2,15 @@ package com.shengsiyuan.jvm.classloader;
 
 /**
  * 主动使用与被动使用.
+ * 对于静态字段而言, 只有直接定义了该字段的类才会被初始化
  *
  * @author RichardLee
  * @version v1.0
  */
 public class MyTest1 {
     public static void main(String[] args) {
-        System.out.println(MyChild1.str);
+//        System.out.println(MyChild1.str);
+        System.out.println(MyChild1.str2);
     }
 }
 
@@ -21,6 +23,9 @@ class MyParent1 {
 }
 
 class MyChild1 extends MyParent1 {
+
+    public static String str2 = "welcome";
+
     static {
         System.out.println("MyChild1 static block");
     }
