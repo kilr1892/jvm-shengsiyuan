037c38e24fb987e11b19dfebf2834bd21c9cff6b
richard_glb <kilr1892@163.com>
2019-11-08 11:42:13 +0800

    第八课例子

---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest5.java
@@ -0,0 +1,28 @@
+package com.shengsiyuan.jvm.classloader;
+
+import java.util.Random;
+
+/**
+ * 接口里的值都是常量
+ * 一个接口在初始化时, 并不要求其父接口都完成了初始化(编译期可确定的值, 删除了 MyParent5 也可以正常执行  话说删除了 MyChild5 也可正常执行)
+ * 只有在真正使用到父接口的时候 (如引用接口中所定义的常量时), 才会初始化
+ */
+public class MyTest5 {
+    public static void main(String[] args) {
+        System.out.println(MyChild5.b);
+
+    }
+}
+
+interface MyParent5 {
+//    public static int a = 5;
+    public static int a = new Random().nextInt(2);
+}
+
+interface MyChild5 extends MyParent5 {
+    //    public static int b = 6;
+//    public static final int b = new Random().nextInt(2);
+//    public static final int b = 5;
+//    public static final int b = new Random().nextInt(4);
+    public static final int b = 5;
+}
---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest6.java
@@ -0,0 +1,45 @@
+package com.shengsiyuan.jvm.classloader;
+
+/**
+ * .
+ *
+ * @author RichardLee
+ * @version v1.0
+ */
+public class MyTest6 {
+    public static void main(String[] args) {
+        // 类的主动使用
+        Singleton singleton = Singleton.getInstance();
+        System.out.println("counter1: " + Singleton.counter1);
+        System.out.println("counter2: " + Singleton.counter2);
+    }
+}
+/*
+ * 1. 连接时的准备阶段
+ *    singleton = null counter1=0 counter2=0
+ * 2. 初始化阶段
+ *    1.1 给 counter1 赋值 为3
+ *    1.2 给 singleton 赋实例 执行了 构造方法
+ *    1.3 给 counter2 赋值为0
+ *
+ * 上述描述可知, 连接是先从上到下把所有变量都准备完, 之后初始化也是从上到下执行完, 注意构造方法是 new 的时候引用的初始化
+ */
+class Singleton {
+    public static int counter1 = 3;
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+        // 这里的是初始化后的值
+        counter1++;
+        // 这里的 counter2 是类在连接时的准备阶段赋予的默认值
+        counter2++;
+    }
+
+    // 这行代码的位置, 可以来理解类的初始化过程
+    // 放在这里, 相当于代码执行到这里之后才进行了初始化 (是变量从上到下写的顺序来执行)
+    public static int counter2 = 0;
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
